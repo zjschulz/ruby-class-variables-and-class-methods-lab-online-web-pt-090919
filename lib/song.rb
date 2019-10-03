617f351a96889a6992c8b911a6330f5da52288c1
Zachary Schulz <zjschulz@gmail.com>
2019-10-03 23:18:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,4 +38,14 @@ class Song
     hash
   end
 
+  def self.artist_count
+     hash = {}
+    @@artists.each {|genre|
+      if hash[genre]
+        hash[genre] = hash[genre] + 1
+      else
+        hash[genre] = 1 
+      end
+    }
+    hash
 end
