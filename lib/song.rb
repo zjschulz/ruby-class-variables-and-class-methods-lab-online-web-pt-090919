8da169faab5a00a493e39d6221a637fbd365ed2b
Zachary Schulz <zjschulz@gmail.com>
2019-10-03 22:40:29 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def genre_count
+    
+  end
+
+end
